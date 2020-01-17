e07a9426d3f1dfccd114c4071307c4e130278381
william175 <bill5986@gmail.com>
2020-01-17 18:06:19 +0800

    修改Footer內容

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,57 @@
-
 import React from "react";
-import { List, ListItem } from "@material-ui/core";
-import Favorite from "@material-ui/icons/Favorite";
+// import Favorite from "@material-ui/icons/Favorite";
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
+import footer2 from '../../contents/imagess/footer/footer-background.png'
+
+const useStyles = makeStyles(theme => ({
+	root: {
+
+		padding: '30px',
+		textAlign: 'center',
+		fontSize: '16px',
+		padding: '20px'
+	},
+	list: {
+		listStyleType: "none",
+		display: 'inline-block',
+		padding: '0 50px',
+
+	},
+	aStyle: {
+
+		textDecoration: 'none'
+	}
+
+
+
+}));
 
 const Footer = (props) => {
+	const classes = useStyles();
 	return (
 		<footer>
-			<div className="footer" style={{}}>
-				<Grid item xs={12}>
-					<ul >
-						<li >
-							<a href="https://www.facebook.com" title="Facebook">Facebook</a>
+			<div className={classes.root}
+				style={{ height: 150, backgroundImage: `url(${footer2})` }}>
+				<Grid item xs={12} style={{ textAlign: 'center' }} >
+					<ul style={{ listStyleType: 'none' }}>
+						<li className={classes.list} >
+							<a href="https://www.facebook.com" target="_blank" title="Facebook" style={{ textDecoration: 'none' }}>Facebook</a>
 						</li >
-						<li >
-							<a href="https://www.instagram.com" title="Instagram">Instagram</a>
+						<li className={classes.list} >
+							<a href="https://www.instagram.com" target="_blank" title="Instagram" style={{ textDecoration: 'none' }}>Instagram</a>
 						</li>
-						<li >
-							<a href="https://github.com/william175pasta" title="GitHub">GitHub</a>
+						<li className={classes.list} >
+							<a href="https://github.com/william175pasta" target="_blank" title="GitHub" style={{ textDecoration: 'none' }}>GitHub</a>
 						</li>
 						<li >
 							&copy; Copy Posted by: william.
-                    </li>
+						</li>
 					</ul>
 				</Grid>
 			</div >
-		</footer>
+		</footer >
 	);
 };
 
