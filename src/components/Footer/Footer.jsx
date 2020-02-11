2ff4a67d31a0a9ffce7ab899787a059aae507792
william175 <bill5986@gmail.com>
2020-02-11 10:53:53 +0800

    修改footer內容

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,29 +3,26 @@ import React from "react";
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
-import footer2 from '../../contents/imagess/footer/footer-background.png'
+import footer2 from '../../img/footer/footer-background.png'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
 
 const useStyles = makeStyles(theme => ({
 	root: {
-
-		padding: '30px',
 		textAlign: 'center',
-		fontSize: '16px',
-		padding: '20px'
+		fontSize: '14px',
+		padding: '50px 0 0',
+		margin: '50px 0 0'
 	},
 	list: {
 		listStyleType: "none",
-		display: 'inline-block',
-		padding: '0 50px',
-
+		textAlign: 'center',
 	},
-	aStyle: {
-
-		textDecoration: 'none'
+	a: {
+		color: '#00c'
 	}
-
-
-
 }));
 
 const Footer = (props) => {
@@ -33,23 +30,41 @@ const Footer = (props) => {
 	return (
 		<footer>
 			<div className={classes.root}
-				style={{ height: 150, backgroundImage: `url(${footer2})` }}>
-				<Grid item xs={12} style={{ textAlign: 'center' }} >
-					<ul style={{ listStyleType: 'none' }}>
+				style={{ backgroundColor: '#ddd' }}>
+				<Grid container spacing={4}>
+					<Grid item xs={12} sm={4}   >
 						<li className={classes.list} >
-							<a href="https://www.facebook.com" target="_blank" title="Facebook" style={{ textDecoration: 'none' }}>Facebook</a>
+							<a href="https://www.facebook.com" target="_blank" title="Facebook" className={classes.a}>
+								<FacebookIcon />
+								Facebook
+							</a>
 						</li >
+					</Grid>
+					<Grid item xs={12} sm={4}   >
 						<li className={classes.list} >
-							<a href="https://www.instagram.com" target="_blank" title="Instagram" style={{ textDecoration: 'none' }}>Instagram</a>
+							<a href="https://www.instagram.com" target="_blank" title="Instagram" className={classes.a}>
+								<InstagramIcon />
+								Instagram
+								</a>
 						</li>
+					</Grid>
+					<Grid item xs={12} sm={4}   >
 						<li className={classes.list} >
-							<a href="https://github.com/william175pasta" target="_blank" title="GitHub" style={{ textDecoration: 'none' }}>GitHub</a>
+							<a href="https://github.com/william175pasta" target="_blank" title="GitHub" className={classes.a}>
+								<GitHubIcon />
+								GitHub
+							</a>
 						</li>
-						<li >
+					</Grid>
+					<Grid item xs={12} sm={12}   >
+						<li className={classes.list} style={{ color: '#000' }} >
 							&copy; Copy Posted by: william.
 						</li>
-					</ul>
+					</Grid>
 				</Grid>
+				{/* <div className={classes.root}
+					style={{ height: 240, backgroundImage: `url(${footer2})` }}>
+				</div> */}
 			</div >
 		</footer >
 	);
